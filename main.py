6382cd1e94e488f8b87ca50c86ed8426c4b1cebf
Just-Iced <plunkettjustice@gmail.com>
2025-06-27 21:23:55 -0700

    Added saving for the card data

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
-from typing import Union
-from pydantic import BaseModel
-
 from scraper import Scraper
 
 app = FastAPI()
-scraper = Scraper()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
+scraper: Scraper = Scraper()
 
 @app.get("/")
 def read_root():
@@ -20,3 +11,5 @@ def read_root():
 @app.get("/stronghold/{card_name}")
 def get_stronghold_card(card_name: str):
     return scraper.scrape(f"https://magicstronghold.com/store/search/{card_name}")
+
+app.add_event_handler("shutdown", scraper.driver.quit)
--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-import selenium
 from selenium.webdriver.common.by import By
 from typing import Union
 from selenium.webdriver import Firefox
@@ -6,20 +5,37 @@ from pydantic import BaseModel
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-from time import sleep
+from time import sleep, time
+import json, jsonpickle
 
 class Card(BaseModel):
     img: str
     link: str
+    site: str
     price: float
     in_stock: bool = True
 
 class Scraper:
-    def __init__(self, sites_formats: Union[dict[str, str], None] = None) -> None:
-        self.site: Union[str, None] = None
-        self.cards = []
+    def __init__(self, 
+                 sites_formats: Union[dict[str, str], None] = None,
+                 data_file: str = "data/card_data.json") -> None:
+        
+        self.site: str = ""
         self.driver: Firefox = Firefox()
+        self.driver.minimize_window()
         self.sites_formats = sites_formats
+
+        self.card_data: dict = {"last_scrape": 0, "cards": []}
+        self.data_file = data_file
+        self.cards: list[Card] = []
+        try:
+            self.card_data = json.load(open(self.data_file))
+            for card_json in self.card_data["cards"]:
+                card: Card = jsonpickle.decode(card_json)
+                self.cards.append(card)
+        except FileNotFoundError:
+            pass
+        
         
     def scrape(self, site: str) -> list[Card]:
         self.cards = []
@@ -31,7 +47,8 @@ class Scraper:
                 self.stronghold_scrape()
             case _:
                 print("Invalid website")
-        print(len(self.cards))
+        self.card_data["last_scrape"] = time()
+        json.dump(self.card_data, open(self.data_file, "w"), indent=4)
         return self.cards
     
     def stronghold_scrape(self) -> None:
@@ -44,7 +61,8 @@ class Scraper:
                 link = "https://magicstronghold.com" + page_card.find_element(By.CLASS_NAME, "searchResultLeftTop").get_dom_attribute("href")
                 price = page_card.find_element(By.CLASS_NAME, "prodPrice").text.strip("$")
                 in_stock = "fa-shopping-cart" in page_card.find_element(By.CLASS_NAME, "quickAddBtn").get_dom_attribute("class")
-                card = Card(img=img, link=link, price=float(price), in_stock=in_stock)
+                card = Card(img=img, site=self.site, link=link, price=float(price), in_stock=in_stock)
+                self.card_data["cards"].append(jsonpickle.encode(card))
                 self.cards.append(card)
             except NoSuchElementException:
                 pass
